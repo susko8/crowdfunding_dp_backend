4add47a6b5dfc96b78c87d7dbbd3f039a0eb34b1
Samuel Šušoliak <samuel.susoliak@artinsolutions.com>
2020-03-06 19:43:19 +0100

    Cors fix

--- src/main/java/com/crowdfunding/dp/backend/config/CORSFilter.java
+++ src/main/java/com/crowdfunding/dp/backend/config/CORSFilter.java
@@ -19,8 +19,8 @@ public class CORSFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE, PATCH");
+        response.setHeader("Access-Control-Allow-Headers", "*");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
         response.setHeader("Access-Control-Expose-Headers", "Location");
         filterChain.doFilter(servletRequest, servletResponse);
     }
